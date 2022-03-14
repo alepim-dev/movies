d9e2b42833f11d3d69ff227970eab0916f8c5f12
alepim <fatec.ale@gmail.com>
2022-03-14 11:34:05 -0300

    show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -2,13 +2,19 @@ import MovieStars from "../MovieStars";
 import "./styles.css";
 
 
-const score =4.5;
-const count =14;
-function MovieScore(){
+type Props={
+    score: number;
+    count: number;
+}
+
+
+function MovieScore({score,count}:Props){
+
+   
 return(
     <div className="dsmovie-score-container">
     <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-    <MovieStars />
+    <MovieStars score={score}/>
     <p className="dsmovie-score-count">{count} avaliações</p>
 </div>
 );
